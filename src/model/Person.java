6b1d5cc084ddb82c4a570e41031335b1d460ba6f
Jesus David Rodriguez Burbano <jesusdavidrodriguezburbano@gmail.com>
2022-06-01 22:49:10 -0500

    Matrix Graph

--- src/model/Person.java
+++ src/model/Person.java
@@ -2,4 +2,12 @@ package model;
 
 public class Person {
 
+	// este id es como un identificador del orden en que se crean los usuarios, es decir que siempre empieza en cero
+	// y va aumentando de uno en uno a media que se crea una persona
+	private int id;
+	
+	public int getId() {
+		return id;
+	}
+	
 }
---
+++ src/util/Matrix_Graph.java
@@ -0,0 +1,46 @@
+package util;
+
+import java.util.HashMap;
+
+public class Matriz_Graph<T> {
+	
+	//Basado en el codigo presentado en este video: https://www.youtube.com/watch?v=kAP8qlzZbeY
+
+	private final int MAX_VERTEX = 100;
+	private HashMap<T, Vertex<T>> vertices;
+	private int edges;
+	private int adjMatrix[][];
+	
+	public Matriz_Graph() {
+		vertices = new HashMap<>();
+		edges = 0;
+		adjMatrix = new int[MAX_VERTEX][MAX_VERTEX];
+		
+		for(int i = 0; i < MAX_VERTEX; i++) {
+			for(int j = 0; j < MAX_VERTEX; j++) {
+				adjMatrix[i][j] = 0;
+			}
+		}
+	}
+	
+	public boolean insert(T o) {
+		if(vertices.get(o) == null) {
+			Vertex<T> v = new Vertex<>(o);
+			vertices.put(o, v);
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	// se le manda por parametro el id de las dos personas que se van a relasionar
+	public void addEdge(int v1, int v2, int weight) {
+		adjMatrix[v1][v2] = weight;
+		edges++;
+	}
+	
+	public Vertex<T> getVertex(T e){
+		return vertices.get(e);
+	}
+	
+}
